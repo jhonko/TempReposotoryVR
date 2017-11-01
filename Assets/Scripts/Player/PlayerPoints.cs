b5681ac702a68bd4df5047a7c42dc37c92cbcdfc
jhohny <j.latenstein.van.voorst@st.hanze.nl>
2017-11-01 14:32:13 +0100

    pushed from hanze PC, added VR shizle

---
+++ Assets/Scripts/Player/PlayerPoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPoints : MonoBehaviour {
+
+    public static float playerScore = 1000f;
+
+    public static float pointsModifier;
+
+
+    public static void MinusPoints()
+    {
+        if (pointsModifier == 1f)
+        {
+            playerScore -= 10f;
+            Debug.Log(playerScore);
+            EndGame();
+        }
+    }
+
+    public static void EndGame()
+    {
+        SceneManager.LoadScene("DeathMenuScene");
+    }
+}
---
+++ Assets/Scripts/Scenes/SceneHandeller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneHandeller : MonoBehaviour {
+
+	void Start () {
+        		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level0.1");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
