4119a9495b1bd4f9e1387de498e87debd64fee85
jhohny <j.latenstein.van.voorst@st.hanze.nl>
2018-03-23 16:59:29 +0100

    fixed vr UI, fixed Eye tracking, made changes to car behaviour

---
+++ Assets/Scripts/Oculus/MirrorTimer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MirrorTimer :MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    //this class keeps track of how long the player looks in a mirror, this can be used to analyse how often and long the player looked in the camera
+
+    public float startTime;
+    public float endTime;
+    public float timeLookedInMirror;
+    public float timesLookedinMirror;
+
+    public Text timeLookingInMirror;
+    public Text timesLookedAtMirror;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("in");
+        startTime = Time.time;
+        timesLookedinMirror++;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        endTime = Time.time;
+        timeLookedInMirror += (endTime - startTime);
+    }
+}
+
--- Assets/Scripts/Player/CarMovement.cs
+++ Assets/Scripts/Player/CarMovement.cs
@@ -8,11 +8,6 @@ public class CarMovement : MonoBehaviour {
     private float speed;
     private float maxSpeed = 700.0f;
 
-    private float steeringAngle;
-    private float steeringInput = 1f;
-    private float maxSteering = 60f;
-    private float steeringNormalizer = 1f;
-
     public float groundDistance;
     public LayerMask layer;
 
@@ -29,6 +24,14 @@ public class CarMovement : MonoBehaviour {
 
     public Rigidbody thisRB;
     public Vector3 cOM;
+
+    // lane swapping
+    private float lane = 25f;
+    private float laneModifier = 2f;
+    private float virtualLane;
+    private bool goLeftInput = false;
+    private bool goRightInput = false;
+    private bool swappingLane = false;
 
 
     void Start () {
@@ -52,6 +55,7 @@ public class CarMovement : MonoBehaviour {
 
     public void UpdateGrounded()
     {
+        // checks if the car is grounded
         if (isGrounded)
         {
             groundDistance = 25f;
@@ -68,6 +72,7 @@ public class CarMovement : MonoBehaviour {
 
     public void CheckForInput()
     {
+        // when the player presses the brake button, there are three speed states for the car 0= standing still, 1= driving normally, 2= driving fast
         if (buttonOneLock == false)
         {
             buttonOneLock = true;
@@ -84,6 +89,7 @@ public class CarMovement : MonoBehaviour {
             }
         }
 
+        // makes sure that the game only gets the input for braking once each time the button is pressed
         if (buttonOneLock == true)
         {
             buttonOneLock = false;
@@ -93,6 +99,7 @@ public class CarMovement : MonoBehaviour {
             }
         }  
 
+        // same as braking but then with acceleration
         if (buttonTwoLock == false) {
                 buttonTwoLock = true;
             if (OVRInput.GetDown(OVRInput.Button.Two))
@@ -117,7 +124,48 @@ public class CarMovement : MonoBehaviour {
             }    
         }
 
-        //steering
+        // lane swapping
+        //right
+        if (OVRInput.GetUp(OVRInput.Button.Four) && swappingLane == false)
+        {
+            goRightInput = true;
+        }
+        if (goRightInput == true && lane < 125)
+        {
+            swappingLane = true;
+            lane += laneModifier;
+            virtualLane += laneModifier;
+            if (virtualLane == 100)
+            {
+                swappingLane = false;
+                goRightInput = false;
+                virtualLane = 0;
+               
+            }
+        }
+
+        //left
+        if (OVRInput.GetUp(OVRInput.Button.Three) && swappingLane == false)
+        {
+            goLeftInput = true;
+        }
+        if (goLeftInput && lane > -175)
+        {
+            Debug.Log("jojo");
+            swappingLane = true;
+            lane -= laneModifier;
+            virtualLane += laneModifier;
+            if (virtualLane == 100)
+            {
+                swappingLane = false;
+                goLeftInput = false;
+                virtualLane = 0;
+                
+            }
+        }
+
+
+        /*//steering
         //right
         if (Input.GetKeyUp("d"))
         {
@@ -135,13 +183,13 @@ public class CarMovement : MonoBehaviour {
         if (Input.GetKeyDown("a"))
         {
             left = true;
-        }
+        }*/
     }
 
     public void ApplyInput()
     {
         speed = Mathf.Clamp(speed, 0, maxSpeed);
-
+        // switch case for the different driving states
         switch (speedState)
         {
             case 0:
@@ -173,7 +221,7 @@ public class CarMovement : MonoBehaviour {
                 break;
         }
 
-        //right
+        /*//right
         if (right && isGrounded && speed != 0)
         {
             steeringAngle += steeringInput;
@@ -204,22 +252,22 @@ public class CarMovement : MonoBehaviour {
                     steeringAngle = 0;
                 }
             }
-        }
-
-        // clamp speeds
-        //steering
-        steeringAngle = Mathf.Clamp(steeringAngle, -maxSteering, maxSteering);
+        }*/
 
         // set speeds to vectors
         //forward
         Vector3 velocity = Vector3.forward * speed;
         //steering
-        Vector3 steering = Vector3.up * steeringAngle;
+        Vector3 laneVector = transform.position;
+        laneVector.x = lane;
 
         // move car
         //forward
         transform.Translate(velocity * Time.deltaTime);
         //steering
-        transform.Rotate(steering * Time.deltaTime);
+        transform.position = laneVector;
+        transform.Rotate(Vector3.up, 0.0f);
+        transform.Translate(velocity * Time.deltaTime);
+
     }
 }
